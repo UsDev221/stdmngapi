763618820b75f70ab138d781b0dbb85a731e47d7
UsDev221 <ushan4487@gmail.com>
2022-03-04 11:51:10 +1030

    Sixth commit

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.DATABASE_URL);
+    const conn = await mongoose.connect(
+      "mongodb+srv://student:student321@cluster0.etwyu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    );
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
     console.log(error);
